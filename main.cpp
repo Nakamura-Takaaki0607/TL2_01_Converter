3bd7c9ada6e795467a53a928f59e0e9c457cacc9
Nakamura-Takaaki0607 <k022g0031@g.neec.ac.jp>
2024-10-15 14:47:24 +0900

    プロジェクト ファイルを追加します。

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <cstdio>
+#include <cstdlib>
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc, char* argv[]) {
+
+	for (int i = 0; i < argc; i++) {
+		printf(argv[i]);
+		printf("\n");
+	}
+
+	/*printf("Hello,World!\n");*/
+
+	system("pause");
+	return 0;
+}
