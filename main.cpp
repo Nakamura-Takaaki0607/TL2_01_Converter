40d714869e58ad35d2267bf20740272caf0a1756
Nakamura-Takaaki0607 <k022g0031@g.neec.ac.jp>
2024-10-15 14:55:30 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -6,11 +6,12 @@
 
 int main(int argc, char* argv[]) {
 
-	for (int i = 0; i < argc; i++) {
+	/*for (int i = 0; i < argc; i++) {
 		printf(argv[i]);
 		printf("\n");
-	}
+	}*/
 
+	printf("Hello,World!\n");
 
 	system("pause");
 	return 0;
